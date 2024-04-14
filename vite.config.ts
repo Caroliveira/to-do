0a01e565e39607b33607d5783d4e620fd728cd73
Carolina Oliveira <linaribeirooliveira@gmail.com>
2024-04-14 18:47:19 -0300

    Configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        name: 'Lina\'s To Do',
+        short_name: 'ToDo',
+        description: 'A very simple and practical To Do App.',
+        theme_color: '#AD4022',
+        icons: [
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "196x196",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      }
+
+    })
+  ],
 })
